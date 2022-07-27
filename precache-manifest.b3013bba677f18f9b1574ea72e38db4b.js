8da5fb38109b25fa97ff5882f5173c07969f2174
Musyoki <vincenbryaniel@gmail.com>
2022-07-27 21:27:15 +0300

    Updates

---
+++ precache-manifest.b3013bba677f18f9b1574ea72e38db4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "963e07b63244b98c47b5",
+    "url": "/airbnb-clone/static/css/main.cb07f5f9.chunk.css"
+  },
+  {
+    "revision": "963e07b63244b98c47b5",
+    "url": "/airbnb-clone/static/js/main.963e07b6.chunk.js"
+  },
+  {
+    "revision": "f396410328fc823aed37",
+    "url": "/airbnb-clone/static/js/1.f3964103.chunk.js"
+  },
+  {
+    "revision": "55bff809efabeb8a7b43",
+    "url": "/airbnb-clone/static/css/2.3c513d70.chunk.css"
+  },
+  {
+    "revision": "55bff809efabeb8a7b43",
+    "url": "/airbnb-clone/static/js/2.55bff809.chunk.js"
+  },
+  {
+    "revision": "289cd78d52b4afa59e93",
+    "url": "/airbnb-clone/static/js/runtime~main.289cd78d.js"
+  },
+  {
+    "revision": "ead8b4ee44f4b884b67e66ed1c233096",
+    "url": "/airbnb-clone/index.html"
+  }
+];
